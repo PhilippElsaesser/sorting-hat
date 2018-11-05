65411ef324f5834f79b2f2892a9ac503c6ff448b
Philipp Elsässer <philipp-elsaesser@outlook.com>
2018-11-05 18:00:03 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from  'react-redux';
+import { createStore } from  'redux';
+import Result from './Containers/result';
+
+const  houses = [
+  'Serpentard',
+  'Griffondor',
+  'Serdaigle',
+  'Pouffsoufle',
+];
+
+const  reducer = (state, action) => {
+  switch(action.type) {
+    case 'UPDATE_CHOICE_WITH_MY_INDEX':
+      return {  
+        houses,
+        selectedHouses:  houses[action.indexSelected]
+      }
+    default:
+      return {  
+        houses,
+        selectedHouses:  houses[Math.floor(Math.random()  *  4)]
+      }
+  }
+    
+
+}
+
+
+const  store = createStore(reducer);
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider  store={store}>
+        <Result  />
+      </Provider>
     );
   }
 }
